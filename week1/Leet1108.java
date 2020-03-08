bc4db22ed115a86f52c55a098a8c90085e05221d
drkim0830 <drkim0830@gmail.com>
2020-03-08 22:12:29 +0900

    week 1 | sangsu's

---
+++ week1/Leet1108.java
@@ -0,0 +1,26 @@
+package day0308;
+
+import java.util.StringTokenizer;
+
+public class Leet1108 {
+	public static void main(String[] args) {
+		System.out.println(defangIPaddr("1.1.1.1"));
+	}
+	/*
+	 * Runtime : 0ms, Memory : 37.3 MB
+	 */
+	public static String defangIPaddr(String address) {
+//		ans = address.replaceAll("\\.", "[.]");	//Time Limit Exceeded
+		String ans = "";
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(address, ".");
+		while(st.hasMoreTokens()) {
+			sb.append(st.nextToken());
+			if(st.hasMoreTokens()) {
+				sb.append("[.]");
+			}
+		}
+		ans = sb.toString();
+		return ans;
+    }
+}
